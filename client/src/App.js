454e5270d2591316153a40737928d149241a854d
Anders Wood <anderswood@gmail.com>
2017-09-14 16:31:29 -0600

    mapmess

--- client/src/App.js
+++ client/src/App.js
@@ -4,27 +4,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 import { polygonParams, polylineParams, drawingMgrProps, initMapOptions } from './mapProps';
+import MapMess from './MapMess';
 
 class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
-
+      myMap: {}
     }
   }
-
 
   componentDidMount () {
     let mapTile = new google.maps.Map(this.refs.mapCanvas, initMapOptions());
     let drawingManager = new google.maps.drawing.DrawingManager(drawingMgrProps());
 
     drawingManager.setMap(mapTile);
+
+    this.setState({myMap: mapTile})
+
   }
 
   render() {
     return (
       <div className="App">
         <div className='GMap-canvas' ref="mapCanvas"></div>
+        <MapMess mapInfo={ this.state.myMap } />
       </div>
     );
   }
